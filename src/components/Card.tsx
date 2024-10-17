16fe8914dcc9cdc83b5f9a425fcc3f0c84c420f3
Saqlain <riyanahmed1703@gmail.com>
2024-10-17 18:44:24 +0530

    fixed type error

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,10 @@
 
 import img from "../assets/poke.jpeg";
-const Card = ({ name, url }) => {
+interface CardProps {
+    name: string;
+    url: string;
+    }
+const Card = ({ name, url }: CardProps) => {
   return (
     <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow">
       <a href={url}>
